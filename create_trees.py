4a3446ab0cbc5001231becd46b6d4e140370dc8c
AndrewQuijano <andrewquijano92@gmail.com>
2021-07-03 00:24:14 -0400

    Will extend path from RST. Then start picking out the paths. Use a do/while to pick the T_0 in next_tree method.

--- create_trees.py
+++ create_trees.py
@@ -2,11 +2,12 @@ from francis import vertex_disjoint_paths, rooted_spanning_tree
 from misc import get_leaves, get_root
 from networkx import DiGraph, get_node_attributes
 from networkx.algorithms.simple_paths import all_simple_edge_paths
+from networkx.classes import is_empty
 from drawing import draw_tree
 import uuid
 
 
-# Create the rooted spanning tree
+# Create the rooted spanning tree (see Francis et al. 2018 paper)
 # args: g - original phylogenetic network
 def create_rooted_tree(g):
     _, paths = vertex_disjoint_paths(g)
@@ -14,62 +15,16 @@ def create_rooted_tree(g):
     return s
 
 
-# Create the Tree t_0 from the Spanning Tree
-# Return Spanning Tree with t_0 removed
+# Starts with a Rooted Spanning Tree (RST) from Francis et al. 2018 paper
+# If there are omnian vertices that are leaves, extend the path to a leaf in N.
 # args:
-# - Spanning Tree of Network N
-# - leaves: leaves of Network N
-# - name: Name of Phylogenetic network to assign name to .png files
-def first_tree(spanning_tree, leaves, name):
-    spanning_tree_copy = spanning_tree.copy()
-    first = DiGraph()
-    root = get_root(spanning_tree)
-    nodes = set()
-
-    # Flip edges for leaf to root?
-    for leaf in leaves:
-        edges = list(all_simple_edge_paths(spanning_tree, root, leaf))[0]
-        # print(edges)
-        # So path is from leaf to root
-        edges.reverse()
-        # print(edges)
-        for src, tgt in edges:
-            first.add_node(src)
-            first.add_node(tgt)
-
-            if spanning_tree.out_degree(tgt) <= 1:
-                nodes.add(tgt)
-            else:
-                break
-
-            if spanning_tree.out_degree(src) <= 1:
-                nodes.add(src)
-            else:
-                break
-        first.add_edges_from(edges)
-        # print("Remove: " + str(nodes))
-        for node in nodes:
-            spanning_tree.remove_node(node)
-        nodes.clear()
-
-    # For illustration
-    draw_tree(spanning_tree_copy, name + '-Spanning-Tree', highlight_edges=first.edges())
-    draw_tree(first, name + "_Tree_0")
-    draw_tree(spanning_tree, name + "_Tree_0_Removed")
-    return first
-
-
-# Starts with Spanning Tree with T_0 removed. All its leaves are unmatched omnian vertices
-# So now I must extend the paths to leaves in Network N.
-# args:
-# Spanning_tree: Spanning Tree with T_0 removed
 # g: Original Phylogenetic Network N
+# Spanning_tree: Spanning Tree with T_0 removed
 def extend_omnian_to_leaves(g, spanning_tree):
-    # Since I may have repeat nodes I need to be careful here as I need the paths to be independent...
     network_leaves = get_leaves(g)
-    unmatched_omnians = get_leaves(spanning_tree)
-    labels = get_node_attributes(spanning_tree, "labels")
-    node_to_uuid = {node_label: uuid_id for uuid_id, node_label in labels.items()}
+    # Map uuid -> node name
+    uuid_to_node_name = get_node_attributes(spanning_tree, "labels")
+    node_to_uuid = {node_label: uuid_id for uuid_id, node_label in uuid_to_node_name.items()}
 
     omnian_to_leaf_paths = list()
 
@@ -81,10 +36,16 @@ def extend_omnian_to_leaves(g, spanning_tree):
 
     # i = 0
     # Path from omnian in S to leaf in G
-    for omnian in unmatched_omnians:
+    for omnian in get_leaves(spanning_tree):
+        # T_0 is NOT removed at first... if the "omnian" is a leaf in N, SKIP!
+        node_name = uuid_to_node_name[omnian]
+        if node_name in network_leaves:
+            # print("Leaf " + str(node_name) + " is NOT an unmatched omnian node!")
+            continue
+
         # Find the paths from Omnian Node to each leaf in N
         for leaf in network_leaves:
-            omnian_to_leaf = list(all_simple_edge_paths(g, labels[omnian], leaf))
+            omnian_to_leaf = list(all_simple_edge_paths(g, uuid_to_node_name[omnian], leaf))
             if len(omnian_to_leaf) == 0:
                 continue
             # If there are is multiple paths to same leaf, just picking any is OK!
@@ -128,14 +89,30 @@ def extend_omnian_to_leaves(g, spanning_tree):
     # Sort by value, I want to prioritize paths that needs more "incoming paths"
     leaf_usage = dict(sorted(leaf_usage.items(), key=lambda item: item[1]))
 
+    for leaf, value in leaf_usage.items():
+        print("Leaf: " + leaf, " Demand: " + str(value))
+
     # Handle the situation of omnian to multiple leaves
     # Because it is sorted, the leaves that require paths come first...
-    for omnian, path_to_leaf in omnian_all_leaf_paths.items():
+    for omnian, path_to_leaves in omnian_all_leaf_paths.items():
+        print("For Omnian: " + uuid_to_node_name[omnian])
+        print("The following paths are available: " + str(path_to_leaves))
+
         # Select which leaf the omnian should go to...
         for target_leaf in leaf_usage.keys():
             # Does the leaf exist in any of the paths?
-            leaf = path_to_leaf[len(path_to_leaf) - 1][1]
-            if target_leaf != leaf:
+            path_to_leaf = None
+            leaf = None
+            for path in path_to_leaves:
+                leaf = path[len(path) - 1][1]
+                print("Path: " + str(path))
+                print("The path has leaf: " + str(leaf))
+                if target_leaf == leaf:
+                    path_to_leaf = path
+                    break
+
+            if path_to_leaf is None:
+                print("The target leaf: " + target_leaf + " has no path...")
                 continue
             # Get Value
             incoming_paths_count = leaf_usage[target_leaf]
@@ -164,6 +141,8 @@ def extend_omnian_to_leaves(g, spanning_tree):
         # To be safe, ensure that the dictionary is sorted by leaf for next paths
         # so the remaining omnians know which path to a leaf should be prioritized
         leaf_usage = dict(sorted(leaf_usage.items(), key=lambda item: item[1]))
+        for leaf, value in leaf_usage.items():
+            print("Leaf: " + leaf, " Demand: " + str(value))
 
 
 # In anticipation you will have multiple node labels,
@@ -186,37 +165,51 @@ def convert_to_uuid_keys(network):
     return new_graph
 
 
-# next tree:
+# Get the next Tree_i created from the Rooted Spanning Tree with Extended Omnian vertex
 # g - complete network N
 # leaves - all leaves in N
 # tree_zero, spanning tree which only has omnian leaves. Used to generate new trees
 # s- spanning tree only containing 1 path to each leaf in N
-def next_tree(tree_zero, spanning_tree):
+def next_tree(tree_zero, spanning_tree, network_leaves):
     nodes = set()
     selected_leaf_set = set()
     tree = DiGraph()
     root = get_root(spanning_tree)
-    network_leaves = get_leaves(tree_zero)
-    unused_leaves = get_leaves(spanning_tree)
-    label_attribute = get_node_attributes(spanning_tree, 'labels')
+    uuid_to_node_name = get_node_attributes(spanning_tree, 'labels')
+
+    # if Tree_0 has no edges (just created). Record first tree crated from this function
+    if is_empty(tree_zero):
+        create_tree_zero = True
+    else:
+        create_tree_zero = False
 
     # Path from omnian in S to leaf in G
-    for leaf in unused_leaves:
+    for leaf in get_leaves(spanning_tree):
+        # print("[NEXT TREE] Select Path: " + uuid_to_node_name[root] + " -> " + uuid_to_node_name[leaf])
         full_path = list(all_simple_edge_paths(spanning_tree, root, leaf))[0]
         full_path.reverse()
+        leaf = uuid_to_node_name[full_path[0][1]]
+        # print("[NEXT TREE] Leaf selected is: " + leaf)
 
-        leaf = label_attribute[full_path[0][1]]
+        # for s, t in full_path:
+        #    print(uuid_to_node_name[s], uuid_to_node_name[t])
+
         # Can't have more than 1 path to same leaf of Network N!
         if leaf in selected_leaf_set:
-            # print("Skipped, Can't Select Same Leaf Twice!")
+            # print("[NEXT TREE] Skipped, Can't Select Same Leaf Twice! " + str(selected_leaf_set))
             continue
 
         selected_leaf_set.add(leaf)
 
-        # print("Nodes in S prime: " + str(list(spanning_tree.nodes())))
+        # Append all paths first...
+        for src, tgt in full_path:
+            if create_tree_zero:
+                tree_zero.add_edge(uuid_to_node_name[src], uuid_to_node_name[tgt])
+            tree.add_edge(uuid_to_node_name[src], uuid_to_node_name[tgt])
+
+        # print("Path is: " + str(full_path))
         # Repeat the same process from earlier
         for src, tgt in full_path:
-            tree.add_edge(label_attribute[src], label_attribute[tgt])
             # print(label_attribute[src] + '->' + label_attribute[tgt])
             if spanning_tree.out_degree(tgt) <= 1:
                 nodes.add(tgt)
@@ -233,12 +226,13 @@ def next_tree(tree_zero, spanning_tree):
             spanning_tree.remove_node(node)
         nodes.clear()
 
-    # print("Leaves picked: " + str(selected_leaf_set))
+    # print("Leaves picked: " + str(selected_leaf_set) + ", First Step of T_i is completed")
 
     # To make it a correct "tree" append all remaining leaves for full leaf set
     # Base this on the first tree generated
     root = get_root(tree_zero)
     for leaf in network_leaves - selected_leaf_set:
+        # print("Create Path from: " + root + " -> " + leaf)
         extra_path = list(all_simple_edge_paths(tree_zero, root, leaf))[0]
         tree.add_edges_from(extra_path)
     return tree
@@ -248,30 +242,37 @@ def next_tree(tree_zero, spanning_tree):
 def enum_trees(g, graph_name, draw=False):
     # Start with creating the spanning tree
     spanning_tree = create_rooted_tree(g)
-    leaves = get_leaves(g)
-    # From Spanning tree, the first tree will just be consist of
-    # spanning tree with all paths to omnians gone
-    # S_prime is the spanning-tree remaining after all first leaves filtered
-    tree_zero = first_tree(spanning_tree, leaves, graph_name)
-    tree_list = [tree_zero]
+    network_leaves = get_leaves(g)
+    # Draw Figure 2(a), of network with edges of Rooted Spanning Tree...
+    draw_tree(g, graph_name + "_RST_Highlighted", highlight_edges=spanning_tree.edges())
+
     # Relabel nodes to UUID keys and labels to allow for multiple instances of label!
+    # Be sure to also extend the path of omnian nodes to a leaf as well
     spanning_tree = convert_to_uuid_keys(spanning_tree)
-
     extend_omnian_to_leaves(g, spanning_tree)
-    draw_tree(spanning_tree, "Graph/Extended-Spanning-Tree-final")
 
-    i = 1
+    # Draw Figure 2(b), of Spanning Tree with the extended network...
+    draw_tree(spanning_tree, graph_name + "_Extended_Spanning_Tree")
+
     # Confirm all nodes all covered at least once
-    all_nodes = set(tree_zero.nodes())
-    # Keep cutting paths from spanning tree S
-    while len(list(spanning_tree.nodes())) != 0:
-        tree = next_tree(tree_zero, spanning_tree)
+    all_nodes = set()
+    tree_zero = DiGraph()
+    i = 0
+    tree_list = []
+
+    # Keep cutting paths from spanning tree S to generate T_i
+    while True:
+        tree = next_tree(tree_zero, spanning_tree, network_leaves)
         tree_list.append(tree)
         if draw:
             draw_tree(tree, graph_name + "_Tree_" + str(i))
         i = i + 1
         all_nodes.update(tree.nodes())
-        # print("Tree made: " + str(list(spanning_tree.nodes())))
+
+        # If there are no edges in Spanning Tree, there are no paths, terminate loop!
+        if is_empty(spanning_tree):
+            break
+
     # Confirm that every node was covered in all the trees...
     assert all_nodes == set(g.nodes())
     return tree_list, len(tree_list)
