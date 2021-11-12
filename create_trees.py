eaf0eb775a4a4e1d6aad9eb629cb6aa0e70a4705
AndrewQuijano <andrewquijano92@gmail.com>
2021-11-12 15:10:33 -0500

    need to see how to print on networkx from .dot format directly

--- create_trees.py
+++ create_trees.py
@@ -174,32 +174,31 @@ def enum_trees(g: DiGraph, graph_name: str, draw=False):
     # Create directory of each tree and place tree 0 dot
     tree_directory = graph_name + '_trees/'
     os.makedirs(tree_directory, exist_ok=True)
-    write_dot(tree_zero, tree_directory + 'tree_0.dot')
 
-    i = 1
     while len(omnian_to_leaf) != 0:
         tree = next_tree(tree_zero, g, spanning_tree, omnian_to_leaf)
         tree_list.append(tree)
-        if draw:
-            # Keep appending dot files for merging with python
-            draw_tree(tree, graph_name + "_Tree_" + str(i))
-            write_dot(tree, tree_directory + 'tree_' + str(i) + '.dot')
-        i = i + 1
         # print("Tree made: " + str(list(spanning_tree.nodes())))
 
     combine_trees(tree_list, tree_directory)
     return tree_list, max(all_incoming_flow)
 
 
+# https://stackoverflow.com/questions/62512760/how-to-label-edges-of-a-multigraph-in-networkx-and-matplotlib
 def combine_trees(trees, tree_dir):
     combined_tree = MultiDiGraph()
-    colors = mcolors.BASE_COLORS.keys()
-    i = 1
+    colors = mcolors.TABLEAU_COLORS.keys()
+    i = 0
     for tree, color in zip(trees, colors):
+        draw_tree(tree, tree_dir + 'tree_' + str(i))
+        write_dot(tree, tree_dir + 'tree_' + str(i) + '.dot')
+        color = color.replace('tab:', '')
         print("Drawing edges with color", color)
         for source, target in tree.edges():
-            combined_tree.add_edge(source, color, color=color)
+            print(source, target, len(combined_tree.edges()))
+            combined_tree.add_edge(source, target, color=color)
         draw_tree(combined_tree, tree_dir + 'combined_graph' + str(i) + '.png')
         i += 1
-    # draw_tree(combined_tree, tree_dir + 'combined_graph.png')
+    write_dot(combined_tree, tree_dir + 'combined_tree.dot')
+    # draw_tree(combined_tree, tree_dir + 'combined_graph')
     return combined_tree
