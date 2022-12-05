9ec791bd56f04a65cf78c21d411513d9916cd3c5
AndrewQuijano <andrewquijano92@gmail.com>
2022-12-05 12:08:05 -0500

    Fixed bug on too many leaves. Also can now print disjoint path to see changes when I use exchange argument

--- treespace/create_trees.py
+++ treespace/create_trees.py
@@ -22,13 +22,13 @@ def iterate_tree(disjoint_paths: list, nodes_used: dict, g) -> DiGraph:
 
     # Essentially similar to spanning tree, but NO omnians though
     for path in disjoint_paths:
-
         # Check if the path ends in a leaf or not...
-        if not path[len(path) - 1] in leaves:
+        if not path[len(path) - 1] in leaves and root not in path:
             continue
 
         # Repeat from Spanning Tree building...
         parents = list(g.predecessors(path[0]))
+
         if root not in path:
             tree.add_edge(parents[0], path[0])
 
@@ -51,6 +51,18 @@ def iterate_tree(disjoint_paths: list, nodes_used: dict, g) -> DiGraph:
             all_roots.remove(remove_node)
         all_roots = all_roots.union(get_all_roots(tree))
 
+    # Make sure you stick to leaf condition too...
+    current_leaves = get_leaves(tree)
+    while leaves != current_leaves:
+        del_leaf = set()
+        for leaf in current_leaves:
+            if leaf not in leaves:
+                tree.remove_node(leaf)
+                del_leaf.add(leaf)
+        for leaf in del_leaf:
+            current_leaves.remove(leaf)
+        current_leaves = current_leaves.union(get_leaves(tree))
+
     # Update Metrics
     for node in tree.nodes():
         counter = nodes_used[node]
@@ -63,8 +75,17 @@ def iterate_tree(disjoint_paths: list, nodes_used: dict, g) -> DiGraph:
 # Essentially, starting from one set of disjoint paths, change the node the omnian should be matched with
 # You should have the same number of disjoint paths, but the new tree should be different and optimize picking
 # more new nodes
-def exchange_disjoint_paths(disjoint_paths: list) -> list:
+def exchange_disjoint_paths(disjoint_paths: list, graph: DiGraph) -> list:
     new_path = []
+    leaves = get_leaves(graph)
+    omnian_paths = []
+    # Break paths ending in omnian and ending in leaf
+    for path in disjoint_paths:
+        if path[len(path) - 1] not in leaves:
+            omnian_paths.append(path)
+
+    # Check what path the omnian node could be matched to now
+    # Could use node counting to determine priority if necessary?
     return new_path
 
 
@@ -90,9 +111,19 @@ def enum_trees(g: DiGraph, graph_name: str, draw=False) -> list:
         trees.append(tree)
         if draw:
             draw_tree(tree, graph_name + '-tree-number-' + str(tree_num))
-            draw_tree(g, graph_name + '-disjoint-paths-' + str(tree_num), highlight_edges=tree.edges())
+            current_disjoint_paths = path_to_edges(paths)
+            draw_tree(g, graph_name + '-disjoint-paths-' + str(tree_num), highlight_edges=current_disjoint_paths)
         # 2- Use exchange algorithm to compute new disjoint paths for next round
-        paths = exchange_disjoint_paths(paths)
+        paths = exchange_disjoint_paths(paths, g)
         break
 
     return trees
+
+
+def path_to_edges(paths: list) -> list:
+    edge_list = []
+    for path in paths:
+        for i in range(len(path) - 1):
+            edge = (path[i], path[i + 1])
+            edge_list.append(edge)
+    return edge_list
