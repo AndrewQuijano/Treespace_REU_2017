7a6267405b9b5a196c130f13509613dd28a8d0d0
Andrew Quijano <andrewfq@amazon.com>
2024-04-23 14:55:48 +0000

    Move answers to test directory

--- test/test_metrics.py
+++ test/test_metrics.py
@@ -9,9 +9,9 @@ from treespace.drawing import draw_tree
 from treespace.create_trees import enum_trees
 
 
-def read_test_answers() -> dict:
+def read_test_answers(file_path: str) -> dict:
     answer_key = dict()
-    with open('answers.csv', 'r') as fd:
+    with open(file_path, 'r') as fd:
         next(fd)
         for line in fd:
             line = line.strip().split(',')
@@ -22,20 +22,21 @@ def read_test_answers() -> dict:
 
 
 class TestTreespace(unittest.TestCase):
-    test_directory = "Graph"
+    graph_directory = "Graph"
+    test_directory = "test"
 
     @classmethod
     def setUpClass(cls):
-        cls.answer = read_test_answers()
-        cls.graph_files = [f for f in os.listdir(cls.test_directory)
-                           if os.path.isfile(os.path.join(cls.test_directory, f))]
+        cls.answer = read_test_answers(os.path.join(cls.test_directory, "answers.csv"))
+        cls.graph_files = [f for f in os.listdir(cls.graph_directory)
+                           if os.path.isfile(os.path.join(cls.graph_directory, f))]
     
     def test_tree_based(self):
         for file_name in self.graph_files:
-            graph = read_adjacency_list(os.path.join(self.test_directory, file_name))
+            graph = read_adjacency_list(os.path.join(self.graph_directory, file_name))
             file_name = file_name.split('.')[0]
             values = self.answer[file_name]
-            t = is_tree_based(graph, os.path.join(self.test_directory, file_name))
+            t = is_tree_based(graph, os.path.join(self.graph_directory, file_name))
             if t:
                 assert values[0] == 1
             else:
@@ -43,7 +44,7 @@ class TestTreespace(unittest.TestCase):
 
     def test_max_cst(self):
         for file_name in self.graph_files:
-            graph = read_adjacency_list(os.path.join(self.test_directory, file_name))
+            graph = read_adjacency_list(os.path.join(self.graph_directory, file_name))
             fname = file_name.split('.')[0]
             values = self.answer[fname]
             _, eta = maximum_covering_subtree(graph, draw=True)
@@ -51,10 +52,10 @@ class TestTreespace(unittest.TestCase):
 
     def test_spanning_tree(self):
         for file_name in self.graph_files:
-            graph = read_adjacency_list(os.path.join(self.test_directory, file_name))
+            graph = read_adjacency_list(os.path.join(self.graph_directory, file_name))
             fname = file_name.split('.')[0]
             values = self.answer[fname]
-            missing_v1, paths = vertex_disjoint_paths(graph, os.path.join(self.test_directory, fname), draw=True)
+            missing_v1, paths = vertex_disjoint_paths(graph, os.path.join(self.graph_directory, fname), draw=True)
             assert values[2] == missing_v1
             spanning_tree = rooted_spanning_tree(graph, paths)
             rooted_tree = tree_based_network(graph, spanning_tree)
@@ -62,10 +63,10 @@ class TestTreespace(unittest.TestCase):
 
     def test_enum_tree(self):
         for file_name in self.graph_files:
-            graph = read_adjacency_list(os.path.join(self.test_directory, file_name))
+            graph = read_adjacency_list(os.path.join(self.graph_directory, file_name))
             fname = file_name.split('.')[0]
             values = self.answer[fname]
-            trees = enum_trees(graph, os.path.join(self.test_directory, fname), draw=True)
+            trees = enum_trees(graph, os.path.join(self.graph_directory, fname), draw=True)
     #        assert values[3] == len(trees)
 
 
